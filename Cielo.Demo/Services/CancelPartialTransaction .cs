64f1815e032018483531cce45294cdcbb5169cd1
Marcos Gonçalves <mgoncalves@useecommerce.com>
2017-09-11 16:58:21 -0300

    Cancel partial transaction demo added

---
+++ Cielo.Demo/Services/CancelPartialTransaction .cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using Cielo.Configuration;
+using Cielo.Responses.Exceptions;
+
+namespace Cielo.Demo.Services
+{
+    public class CancelpartialTransaction : ICieloDemo
+    {
+        private ICieloDemoInterface _app;
+
+        public static CieloDemoInfo Info
+        {
+            get
+            {
+                var info = new CieloDemoInfo(
+                    typeof(CancelTransaction),
+                    "cancel-partial",
+                    "Cancel partialy a transaction",
+                    "");
+                return info;
+            }
+        }
+
+        #region ICieloDemo Members
+
+        public void Execute(ICieloDemoInterface app)
+        {
+            this._app = app;
+            this.GetArgs();
+            this.ExecuteRequest();
+        }
+
+        #endregion
+
+        private void GetArgs()
+        {
+            foreach (var arg in _app.Args)
+            {
+
+            }
+        }
+
+        private void ExecuteRequest()
+        {
+            var watch = Stopwatch.StartNew();
+            ConsoleHelper.WriteHeader("Canceling a Transaction");
+
+            CustomConfiguration configuration = new CustomConfiguration()
+            {
+                DefaultEndpoint = ConfigurationManager.AppSettings["cielo.endpoint.default"],
+                QueryEndpoint = ConfigurationManager.AppSettings["cielo.endpoint.query"],
+                MerchantId = ConfigurationManager.AppSettings["cielo.customer.id"],
+                MerchantKey = ConfigurationManager.AppSettings["cielo.customer.key"],
+                ReturnUrl = ConfigurationManager.AppSettings["cielo.return.url"],
+            };
+
+            CieloService cieloService = new CieloService(configuration);
+
+            try
+            {
+                var response = cieloService.CancelTransaction(paymentId: Guid.Parse("1a2d178a-dc87-4627-92a2-b5d18ba076cd"), amount: 10m);
+
+                ConsoleHelper.WriteResult(response);
+            }
+            catch (ResponseException ex)
+            {
+                ConsoleHelper.WriteError(ex);
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelper.WriteError(ex);
+            }
+
+            watch.Stop();
+            ConsoleHelper.WriteFooter(watch.ElapsedMilliseconds);
+        }
+    }
+}
